0e4b8f0e3d2dd1778071ea79346b70e5813b5095
aaliyahc05 <143969790+aaliyahc05@users.noreply.github.com>
2024-04-05 13:20:33 -0600

    hi

--- wa/main1.js
+++ wa/main1.js
@@ -5,15 +5,30 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-
-/* Declaring the alternative text for each image file */
+const alts =["nelly","jelly","sky","serena","hailey","bella"];
 
 /* Looping through images */
-
-
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+    function pictureLoop(){
+        var i; 
+        for(i = 0; i < 6; i++){
+            var newImage = document.createElement('img');
+            newImage.setAttribute('src',"img" + [i+1] + ".JPG/jpeg");
+            newImage.setAttribute('alt', alts[i]);
+            thumbBar.appendChild(newImage);
+        }
+    }
 
 /* Wiring up the Darken/Lighten button */
+
+btn.addEventListener('click', () => {
+  const btnClass = btn.getAttribute('class');
+  if (btnClass === 'dark') {
+    btn.setAttribute('class','light');
+    btn.textContent = 'Lighten';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+  } else {
+    btn.setAttribute('class','dark');
+    btn.textContent = 'Darken';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+  }
+});
