178d120837bfb5fce97f5d9eb72cf71427b3626d
aaliyahc05 <143969790+aaliyahc05@users.noreply.github.com>
2024-04-11 21:39:47 -0600

    hi

--- ica/ica12.js
+++ ica/ica12.js
@@ -1,17 +1,62 @@
+// const btn = document.querySelector("#js-new-quote");
+// btn.addEventListener('click', getQuote);
+
+// const answerBtn = document.querySelector("#js-tweet");
+// answerBtn.addEventListener('click', getAnswer);
+
+// const answerText = document.querySelector("#js-answer-text");
+
+// let answer = '';
+
+
+// const endpoint = "https://trivia.cyberwisp.com/getrandomchristmasquestion";
+
+// async function getQuote(){
+//     // console.log("Test")
+//     try{
+//         const response = await fetch(endpoint);
+//         if(!response.ok){
+//             throw Error(response.statusText)
+//         }
+
+//         const json = await response.json();
+//         console.log(json['question']);
+//         displayQuote(json['question']);
+//         console.log(json['answer']);
+//         answer = json['answer'];
+//         answerText.textContent =Z
+//     } catch (err){
+//         console.log(err);
+//         alert('Failed to fetch new quote');
+//     }
+// }
+
+// function displayQuote(quote){
+//     const quoteText = document.querySelector("#js-quote-text");
+//     quoteText.textContent = quote;
+// }
+
+// function getAnswer(){
+//     console.log(212);
+//     answerText.textContent = answer; 
+// }
+
+// getQuote();
+
 const btn = document.querySelector("#js-new-quote");
-btn.addEventListener('click', getQuote);
+btn.addEventListener('click', getFact);
 
-const answerBtn = document.querySelector("#js-tweet");
-answerBtn.addEventListener('click', getAnswer);
+// const answerBtn = document.querySelector("#js-tweet");
+// answerBtn.addEventListener('click', getAnswer);
 
-const answerText = document.querySelector("#js-answer-text");
+// const answerText = document.querySelector("#js-answer-text");
 
-let answer = '';
+// let answer = '';
 
 
-const endpoint = "https://trivia.cyberwisp.com/getrandomchristmasquestion";
+const endpoint = "https://cat-fact.herokuapp.com/facts/random";
 
-async function getQuote(){
+async function getFact(){
     // console.log("Test")
     try{
         const response = await fetch(endpoint);
@@ -20,25 +65,20 @@ async function getQuote(){
         }
 
         const json = await response.json();
-        console.log(json['question']);
-        displayQuote(json['question']);
-        console.log(json['answer']);
-        answer = json['answer'];
-        answerText.textContent = '';
+        // console.log(json['fact']);
+        // displayFact(json['fact']);
+        // console.log(json['answer']);
+        // answer = json['answer'];
+        // answerText.textContent = ''
     } catch (err){
         console.log(err);
         alert('Failed to fetch new quote');
     }
 }
 
-function displayQuote(quote){
-    const quoteText = document.querySelector("#js-quote-text");
-    quoteText.textContent = quote;
+function displayFact(fact){
+    const factText = document.querySelector("#js-quote-text");
+    factText.textContent = fact;
 }
 
-function getAnswer(){
-    console.log(212);
-    answerText.textContent = answer; 
-}
-
-getQuote();
+getFact();
