3d022cd072b7886efddeb438db4e2c5d78b2c30c
aaliyahc05 <143969790+aaliyahc05@users.noreply.github.com>
2024-04-14 21:33:24 -0600

    hi

--- ica/ica12.js
+++ ica/ica12.js
@@ -1,84 +1,70 @@
-// const btn = document.querySelector("#js-new-quote");
-// btn.addEventListener('click', getQuote);
-
-// const answerBtn = document.querySelector("#js-tweet");
-// answerBtn.addEventListener('click', getAnswer);
-
-// const answerText = document.querySelector("#js-answer-text");
-
-// let answer = '';
-
-
-// const endpoint = "https://trivia.cyberwisp.com/getrandomchristmasquestion";
-
-// async function getQuote(){
-//     // console.log("Test")
-//     try{
-//         const response = await fetch(endpoint);
-//         if(!response.ok){
-//             throw Error(response.statusText)
-//         }
-
-//         const json = await response.json();
-//         console.log(json['question']);
-//         displayQuote(json['question']);
-//         console.log(json['answer']);
-//         answer = json['answer'];
-//         answerText.textContent =Z
-//     } catch (err){
-//         console.log(err);
-//         alert('Failed to fetch new quote');
-//     }
-// }
-
-// function displayQuote(quote){
-//     const quoteText = document.querySelector("#js-quote-text");
-//     quoteText.textContent = quote;
-// }
-
-// function getAnswer(){
-//     console.log(212);
-//     answerText.textContent = answer; 
-// }
-
-// getQuote();
-
 const btn = document.querySelector("#js-new-quote");
-btn.addEventListener('click', getFact);
+btn.addEventListener('click', getQuote);
 
-// const answerBtn = document.querySelector("#js-tweet");
-// answerBtn.addEventListener('click', getAnswer);
+const answerBtn = document.querySelector("#js-tweet");
+answerBtn.addEventListener('click', shareQuote);
 
-// const answerText = document.querySelector("#js-answer-text");
+const answerText = document.querySelector("#js-answer-text");
 
-// let answer = '';
+let answer = '';
 
 
-const endpoint = "https://cat-fact.herokuapp.com/facts/random";
+// const endpoint = "https://wordsapiv1.p.rapidapi.com/words/hatchback/typeOf";
 
-async function getFact(){
-    // console.log("Test")
-    try{
-        const response = await fetch(endpoint);
-        if(!response.ok){
-            throw Error(response.statusText)
-        }
+async function getQuote(){
+    const axios = require('axios');
 
-        const json = await response.json();
-        // console.log(json['fact']);
-        // displayFact(json['fact']);
-        // console.log(json['answer']);
-        // answer = json['answer'];
-        // answerText.textContent = ''
-    } catch (err){
-        console.log(err);
-        alert('Failed to fetch new quote');
+    const options = {
+      method: 'GET',
+      url: 'https://wordsapiv1.p.rapidapi.com/words/hatchback/typeOf',
+      headers: {
+        'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
+        'X-RapidAPI-Host': 'wordsapiv1.p.rapidapi.com'
+      }
+    };
+    
+    try {
+        const response = await axios.request(options);
+        console.log(response.data);
+    } catch (error) {
+        console.error(error);
     }
 }
 
-function displayFact(fact){
-    const factText = document.querySelector("#js-quote-text");
-    factText.textContent = fact;
+function displayQuote(quote){
+    const quoteText = document.querySelector("#js-quote-text");
+    quoteText.textContent = quote;
 }
 
-getFact();
+const twitterURL = "https://twitter.com/intent/tweet/";
+const linkTarget = '_top';
+const windowOptions = 'menubar=no,status=no,height=750,width=500';
+
+function extractQuote(){
+console.log(document.querySelector('#js-quote-text').textContent);
+return document.querySelector('#js-quote-text').textContent;
+
+}
+
+function extractAuthor(){
+console.log(document.querySelector('#js-quote-author').textContent);
+return document.querySelector('#js-quote-author').textContent;
+}
+
+function openTwitterWindow() {
+const quote = extractQuote();
+const author = extractAuthor();
+const twitterQuery = `text=${encodeURIComponent(`"${quote}" - ${author}`)}&url=`;
+return window.open(`${twitterURL}?${twitterQuery}&`, linkTarget, windowOptions);
+}
+
+function shareQuote() {
+const twitterButton = document.querySelector('#js-tweet-button');
+twitterButton.addEventListener('click', () => openTwitterWindow());
+}
+
+shareQuote();
+
+getQuote();
+
+
